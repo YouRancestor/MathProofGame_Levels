7399cb72f704d9e1fabbf4bc844b5f6e369686c7
zyr <zyr_zyr_@sohu.com>
2024-08-05 23:21:31 +0800

    添加战术矛盾（contradiction）教程关

---
+++ Tutorial/contradiction.lean
@@ -0,0 +1,9 @@
+import Tutorial.Notation
+import Definition.Tactic
+
+/--
+### 矛盾（contradiction）
+当假设中含有一对矛盾的命题 p 和 ¬p 时，可以使用“矛盾”来完成证明。
+-/
+example (p q : Prop) (h : p) (h' : ¬p) : q := by
+  contradiction
