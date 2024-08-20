9f966048f9c03d9c9257b2de474be3e1ae842b00
ChaosLeizure <chaosleizure@outlook.com>
2024-08-20 22:28:07 +0800

    更新constructor教程的注释说明

--- Collection/Basic/Tutorial/constructor_and.lean
+++ Collection/Basic/Tutorial/constructor_and.lean
@@ -1,7 +1,7 @@
 
 /--
-### 拆分（且）(constructor)
-对于且型目标 P ∧ Q ，使用**拆分**将其分解为 P 和 Q 两个子目标，然后分别证明。
+### 分解（且）(constructor)
+对于且型目标 P ∧ Q ，使用**分解**将 ∧ 号连接的 P 和 Q 构造为两个子目标，然后分别证明。
 -/
 example (P Q : Prop) (p : P) (q : Q) : P ∧ Q := by
   constructor
--- Collection/Basic/Tutorial/constructor_iff.lean
+++ Collection/Basic/Tutorial/constructor_iff.lean
@@ -1,7 +1,7 @@
 
 /--
-### 拆分（当且仅当）(constructor)
-对于“当且仅当（iff）”（if and only if）型目标 P ↔ Q ，使用**拆分**将其分解为为 P 和 Q 两个子目标，然后分别证明。
+### 分解（等价）(constructor)
+对于等价（当且仅当）型目标 P ↔ Q ，使用**分解**将 ↔ 号连接的 P 和 Q 构造为两个子目标，然后分别证明。
 -/
 example : P ∧ Q ↔ Q ∧ P := by
   constructor
