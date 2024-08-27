d1c2276930beabe4760de33fe388df7f58702d61
ChaosLeizure <chaosleizure@outlook.com>
2024-08-27 23:53:43 +0800

    更新cases和by_cases的注释说明

--- Collection/Basic/Tutorial/by_cases.lean
+++ Collection/Basic/Tutorial/by_cases.lean
@@ -2,8 +2,10 @@ import Definition.Basic.Tutorial.Notation
 import Definition.Basic.Tactic
 
 /--
-### （by_cases）
+### 讨论（命题）（by_cases）
 引入一个命题，对其肯定形式和否定形式分别进行讨论。
+#### 提示
+x ≠ 1 与 ¬(x = 1) 以及 x = 1 → False 是完全等价的，它们只是同一表达式的不同记法。
 -/
 example (x : ℕ) : x = 1 ∨ x ≠ 1 := by
   let h1 := x = 1 -- 通过点击x=1创建一张卡片
