f330675b45a1964139c7b6cc3d337b85153ad82a
ChaosLeizure <chaosleizure@outlook.com>
2025-04-26 08:42:27 +0800

    命题mul_one添加'?'后缀

--- Collection/Basic/Arithmetic/mul_one.lean
+++ Collection/Basic/Arithmetic/mul_one.lean
@@ -4,7 +4,7 @@ import Definition.Basic.Tactic
 import Collection.Basic.Arithmetic.zero_add
 
 open Nat
-theorem mul_one (a : ℕ) : a * 1 = a := by
+theorem mul_one'?' (a : ℕ) : a * 1 = a := by
   rewrite [one_eq_succ_zero'?']
   rewrite [mul_succ'?' a]
   rewrite [mul_zero'?' a]
